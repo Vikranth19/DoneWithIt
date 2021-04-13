ace9e51c0da50d644d514c0157cc0dd8187eb2c5
Vikranth Paluri <vikranthpaluri@gmail.com>
2021-04-14 00:56:55 +0530

    added all the files

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from '@react-native-community/hooks'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  SafeAreaView,
+  Image,
+  Alert,
+  Button,
+  Platform,
+} from 'react-native'
+
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+import AppText from './app/components/AppText/AppText'
+import WelcomeScreen from './app/screens/WelcomeScreen'
+import AppButton from './app/components/AppButton'
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen'
+import ViewImageScreen from './app/screens/ViewImageScreen'
+import MessagesScreen from './app/screens/MessagesScreen'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  // console.log(Dimensions.get('screen'))
+  // console.log(useDimensions())
+  // console.log(useDeviceOrientation())
+
+  const { landscape } = useDeviceOrientation()
+
+  return <MessagesScreen />
 }
 
+//does not validate properties
+const containerStyle = { backgroundColor: 'orange' }
+
+//validates the properties, but just plain js object doesn't ensure validity
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
-});
+})
---
+++ app/components/AppText/styles.js
@@ -0,0 +1,10 @@
+//Platform.select returns an object
+import { Platform, StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
+  },
+})
+
+export default styles
